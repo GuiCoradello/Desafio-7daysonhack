76f4504a59156ef8f1396f69860746d0de5844b6
GuiCoradello <coradello.silveira@gmail.com>
2022-06-22 00:38:32 -0300

    Quinto Dia Desafio

---
+++ quintoDia/quintoDia.js
@@ -0,0 +1,15 @@
+const frutas = document.getElementById('frutas')
+const laticinios = document.getElementById('laticinios');
+const doces = document.getElementById('doces');
+const naoPereciveis = document.getElementById('nao-Perecivel')
+const boaVInda = document.getElementById('boaVinda')
+
+function Mostrafrutas(){
+ 
+}
+
+
+
+
+
+
